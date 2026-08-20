77d7841dc7ab7c8e02310f218167554e007803bc
velikiievgeniusultimate <velikiievgeniusultimate@gmail.com>
2026-08-20 13:16:16 +0300

    Restore one-slot spectator selector item

--- src/main/java/org/evgenium/speedrun/spectator/SpectatorServerHooks.java
+++ src/main/java/org/evgenium/speedrun/spectator/SpectatorServerHooks.java
@@ -14,6 +14,8 @@ public final class SpectatorServerHooks {
                 return;
             }
             player.getInventory().clearContent();
+            player.getInventory().setItem(0, SpectatorItems.createSelector());
+            player.getInventory().setSelectedSlot(0);
         });
     }
 }
