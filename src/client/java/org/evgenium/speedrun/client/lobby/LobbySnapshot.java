f8a722ff5064b0202ce67f35ad348c2002ef9a47
velikiievgeniusultimate <velikiievgeniusultimate@gmail.com>
2026-08-20 10:06:03 +0300

    Include selected goal in lobby snapshot

--- src/client/java/org/evgenium/speedrun/client/lobby/LobbySnapshot.java
+++ src/client/java/org/evgenium/speedrun/client/lobby/LobbySnapshot.java
@@ -2,12 +2,15 @@ package org.evgenium.speedrun.client.lobby;
 
 import java.util.List;
 
-public record LobbySnapshot(List<LobbyPlayer> players, boolean cheatsEnabled) {
+public record LobbySnapshot(List<LobbyPlayer> players, boolean cheatsEnabled, SpeedrunGoal goal) {
     public LobbySnapshot {
         players = List.copyOf(players);
+        if (goal == null) {
+            goal = SpeedrunGoal.COMPLETE_MINECRAFT;
+        }
     }
 
     public static LobbySnapshot empty() {
-        return new LobbySnapshot(List.of(), false);
+        return new LobbySnapshot(List.of(), false, SpeedrunGoal.COMPLETE_MINECRAFT);
     }
 }
