66c07785b266302d3c9ca053ec26314333565bbc
velikiievgeniusultimate <velikiievgeniusultimate@gmail.com>
2026-08-20 09:24:40 +0300

    Track speedrun world join state

--- src/client/java/org/evgenium/speedrun/client/EvgeniumSpeedRunClient.java
+++ src/client/java/org/evgenium/speedrun/client/EvgeniumSpeedRunClient.java
@@ -1,7 +1,9 @@
 package org.evgenium.speedrun.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import org.evgenium.speedrun.EvgeniumSpeedRun;
+import org.evgenium.speedrun.client.runtime.ClientPhase;
 import org.evgenium.speedrun.client.runtime.ClientRuntime;
 import org.evgenium.speedrun.client.ui.MenuRouter;
 
@@ -10,6 +12,11 @@ public final class EvgeniumSpeedRunClient implements ClientModInitializer {
     public void onInitializeClient() {
         ClientRuntime.initialize();
         MenuRouter.install();
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            if (ClientRuntime.phase() == ClientPhase.PREPARING_WORLD) {
+                ClientRuntime.transitionTo(ClientPhase.RUNNING);
+            }
+        });
         EvgeniumSpeedRun.LOGGER.info("Evgenium SpeedRun client initialized");
     }
 }
