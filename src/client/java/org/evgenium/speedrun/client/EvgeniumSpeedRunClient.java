e3d874936cdb655074c478eb95ddf9d7a879a2bd
velikiievgeniusultimate <velikiievgeniusultimate@gmail.com>
2026-08-20 09:42:52 +0300

    Install synchronized race barrier and timer HUD

--- src/client/java/org/evgenium/speedrun/client/EvgeniumSpeedRunClient.java
+++ src/client/java/org/evgenium/speedrun/client/EvgeniumSpeedRunClient.java
@@ -1,22 +1,23 @@
 package org.evgenium.speedrun.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import org.evgenium.speedrun.EvgeniumSpeedRun;
-import org.evgenium.speedrun.client.runtime.ClientPhase;
-import org.evgenium.speedrun.client.runtime.ClientRuntime;
+import org.evgenium.speedrun.client.match.RaceSession;
+import org.evgenium.speedrun.client.match.SpeedrunTimerHud;
 import org.evgenium.speedrun.client.ui.MenuRouter;
 
 public final class EvgeniumSpeedRunClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ClientRuntime.initialize();
+        org.evgenium.speedrun.client.runtime.ClientRuntime.initialize();
         MenuRouter.install();
-        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-            if (ClientRuntime.phase() == ClientPhase.PREPARING_WORLD) {
-                ClientRuntime.transitionTo(ClientPhase.RUNNING);
-            }
-        });
+        SpeedrunTimerHud.install();
+
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> RaceSession.onWorldJoined(client));
+        ClientTickEvents.END_CLIENT_TICK.register(RaceSession::tick);
+
         EvgeniumSpeedRun.LOGGER.info("Evgenium SpeedRun client initialized");
     }
 }
