7686a76518e9a892e1b3eaf4d78cf4cfb5797c05
velikiievgeniusultimate <velikiievgeniusultimate@gmail.com>
2026-08-20 10:48:16 +0300

    Restore spectator selector on joined worlds

---
+++ src/main/java/org/evgenium/speedrun/spectator/SpectatorServerHooks.java
@@ -0,0 +1,21 @@
+package org.evgenium.speedrun.spectator;
+
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.level.ServerPlayer;
+
+public final class SpectatorServerHooks {
+    private SpectatorServerHooks() {
+    }
+
+    public static void install() {
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            ServerPlayer player = handler.getPlayer();
+            if (!player.isSpectator()) {
+                return;
+            }
+            player.getInventory().clearContent();
+            player.getInventory().setItem(0, SpectatorItems.createSelector());
+            player.getInventory().setSelectedSlot(0);
+        });
+    }
+}
