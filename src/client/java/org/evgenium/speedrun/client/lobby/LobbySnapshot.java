52e67c944310e618e2f6fc05adf076c8e8f38e01
velikiievgeniusultimate <velikiievgeniusultimate@gmail.com>
2026-08-20 09:23:03 +0300

    Include debug rules in lobby state

--- src/client/java/org/evgenium/speedrun/client/lobby/LobbySnapshot.java
+++ src/client/java/org/evgenium/speedrun/client/lobby/LobbySnapshot.java
@@ -2,12 +2,12 @@ package org.evgenium.speedrun.client.lobby;
 
 import java.util.List;
 
-public record LobbySnapshot(List<LobbyPlayer> players) {
+public record LobbySnapshot(List<LobbyPlayer> players, boolean cheatsEnabled) {
     public LobbySnapshot {
         players = List.copyOf(players);
     }
 
     public static LobbySnapshot empty() {
-        return new LobbySnapshot(List.of());
+        return new LobbySnapshot(List.of(), false);
     }
 }
