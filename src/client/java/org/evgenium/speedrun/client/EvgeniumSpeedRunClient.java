36904ad4f8b6a25d27d6b255a5fc2d9337fe0bb6
velikiievgeniusultimate <velikiievgeniusultimate@gmail.com>
2026-08-20 10:09:28 +0300

    Detect vanilla end credits as completed-game finish

--- src/client/java/org/evgenium/speedrun/client/EvgeniumSpeedRunClient.java
+++ src/client/java/org/evgenium/speedrun/client/EvgeniumSpeedRunClient.java
@@ -3,6 +3,8 @@ package org.evgenium.speedrun.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.client.gui.screens.WinScreen;
 import org.evgenium.speedrun.EvgeniumSpeedRun;
 import org.evgenium.speedrun.client.match.RaceSession;
 import org.evgenium.speedrun.client.match.SpeedrunTimerHud;
@@ -17,6 +19,11 @@ public final class EvgeniumSpeedRunClient implements ClientModInitializer {
 
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> RaceSession.onWorldJoined(client));
         ClientTickEvents.END_CLIENT_TICK.register(RaceSession::tick);
+        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+            if (screen instanceof WinScreen) {
+                RaceSession.onWinScreenOpened();
+            }
+        });
 
         EvgeniumSpeedRun.LOGGER.info("Evgenium SpeedRun client initialized");
     }
